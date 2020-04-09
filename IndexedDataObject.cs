3b58071306de63811a98cadd1df4aa3de9d7501c
alice-hekrdlova <61107141+alice-hekrdlova@users.noreply.github.com>
2020-04-09 07:52:43 +0200

    Add files via upload

---
+++ IndexedDataObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace GZipTestProject
+{   
+    /// <summary>
+    /// Wrapper class containing data (either compressed or decompressed) and an index ensuring correct order of data being written to the output file later on.
+    /// </summary>
+    
+    [Serializable]
+    public class IndexedDataObject
+    {
+        public byte[] DataBlock { get; set; }
+
+        /// <summary>
+        /// Index defines the precize order in which this data block should be written to the output file 
+        /// (in order to be able to read the data from the file later on and reconstruct the original file data).
+        /// </summary>
+        public uint Index { get; set; }
+
+        public IndexedDataObject(byte[] block, uint i)
+        {
+            DataBlock = block;
+            Index = i;
+        }
+
+ 
+    }
+
+}
