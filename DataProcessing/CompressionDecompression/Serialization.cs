3507fcbe5bf586cf92a8635f40fdd68d7c175cac
alice-hekrdlova <61107141+alice-hekrdlova@users.noreply.github.com>
2020-04-09 07:48:47 +0200

    Add files via upload

---
+++ DataProcessing/CompressionDecompression/Serialization.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace GZipTestProject
+{
+    /// <summary>
+    /// Serializes a compressed data object to the given file stream.
+    /// </summary>
+    public class Serialization
+    {
+        /// <summary>
+        /// Serializes a compressed data object to the given file stream.
+        /// </summary>
+        /// <param name="compressedDataObject">object with a compressed byte[] and corresponding index</param>
+        /// <param name="outputStream">output file stream</param>
+        public static void Serialize(IndexedDataObject compressedDataObject, FileStream outputStream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(outputStream, compressedDataObject);
+        }
+    }
+}
+ 
