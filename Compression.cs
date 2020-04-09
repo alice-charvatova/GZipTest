98b65fabb4eb0192772f8ca04b95ec7b23ffb648
alice-hekrdlova <61107141+alice-hekrdlova@users.noreply.github.com>
2020-04-09 07:34:44 +0200

    Add files via upload

---
+++ Compression.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+
+namespace GZipTestProject
+{
+    /// <summary>
+    /// Takes a byte array and returns it compressed (again as a byte array).
+    /// </summary>
+    public class Compression
+    {
+        /// <summary>
+        /// Takes a byte array and returns it compressed (again as a byte array).
+        /// </summary>
+        /// <param name="data">original byte array</param>
+        /// <returns>compressed byte array</returns>
+        public static byte[] Compress(byte[] data)     
+        {
+            using (var compressedStream = new MemoryStream())
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            {
+                zipStream.Write(data, 0, data.Length);   
+                zipStream.Close();
+                return compressedStream.ToArray();    // Converts zipped data from memory stream to a byte array.
+            }
+        }
+    }
+}
