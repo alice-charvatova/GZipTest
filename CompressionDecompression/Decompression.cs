73e287962465c8f3316d2ead22d5435b8a28afe1
alice-hekrdlova <61107141+alice-hekrdlova@users.noreply.github.com>
2020-04-09 07:44:35 +0200

    Add files via upload

---
+++ CompressionDecompression/Decompression.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+
+namespace GZipTestProject
+{
+    /// <summary>
+    /// Takes a byte array (with compressed data), decompress it and returns it as a byte array.
+    /// </summary>
+    public class Decompression
+    {
+        /// <summary>
+        /// Takes a byte array (with compressed data), decompress it and returns it as a byte array.
+        /// </summary>
+        /// <param name="compressedData">compressed data byte array</param>
+        /// <returns>decompressed byte array</returns>
+        public static byte[] Decompress(byte[] compressedData) //Writes an input byte array into a memory stream, then GZipStream (for decompression), then into memory stream again for converting data into a byte array.
+        {
+            using (var compressedStream = new MemoryStream(compressedData))
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (var resultStream = new MemoryStream())
+            {
+                zipStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+    }
+
+
+
+}
