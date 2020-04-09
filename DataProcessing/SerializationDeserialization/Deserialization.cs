7c796494513d308e77e9ecddfa0819855384a404
alice-hekrdlova <61107141+alice-hekrdlova@users.noreply.github.com>
2020-04-09 07:51:09 +0200

    Add files via upload

---
+++ DataProcessing/SerializationDeserialization/Deserialization.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace GZipTestProject
+{
+    /// <summary>
+    /// Deserializes a compressed data object from the given file stream.
+    /// </summary>
+    public class Deserialization
+    {
+        /// <summary>
+        /// Deserializes a compressed data object from the given file stream.
+        /// </summary>
+        /// <param name="inputStream">input file stream</param>
+        /// <returns>object with a compressed byte[] and corresponding index</returns>
+        public static IndexedDataObject Deserialize(FileStream inputStream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            IndexedDataObject compressedDataObject = (IndexedDataObject)formatter.Deserialize(inputStream);
+            
+            return compressedDataObject;
+        }
+    }
+}
+ 
